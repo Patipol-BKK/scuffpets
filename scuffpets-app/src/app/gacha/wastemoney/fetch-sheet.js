0abf7cb59bcf74594513ec005f70c47a05291312
Tom Li <tom.xq.li@gmail.com>
2023-10-05 20:55:08 -0700

    Add searching for image when fetching sheet

--- scuffpets-app/src/app/gacha/wastemoney/fetch-sheet.js
+++ scuffpets-app/src/app/gacha/wastemoney/fetch-sheet.js
@@ -1,23 +1,36 @@
 import axios from 'axios'
-import fs from 'fs'
+import { promises as fs } from 'fs';
+import util from 'util'
 
+const readFile = util.promisify(fs.readFile);
+
+// Fetch data from google sheets
 export async function fetchGoogleSheets() {
+  // Google Sheets API consts
   const SHEET_ID = process.env.GACHA_SHEET_ID
   const SHEET_NAME = 'gachas'
   const API_KEY = process.env.SHEET_API_KEY
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_NAME}?valueRenderOption=FORMATTED_VALUE&key=${API_KEY}`
 
+  // image loading consts
+  const IMAGE_FORMATS = ['.jpg', '.jpeg', '.png', '.webp'];
+  const IMAGE_PATH = ['/img/gachapics/']
+  const ERROR_IMAGE = 'https://images-ext-2.discordapp.net/external/8yASqYbZYWRCIK_14U-tZwCglJZ7DGNrWB94DASuRTk/https/cdn.discordapp.com/emojis/955646373246672966.png?width=160&height=160'
+
   // Get sheet values
   axios.get(url)
-  .then(function (response) {
-      // Handle success
+    .then(function (response) {
+      // Handle fetch success
       var sheetResponse = response.data.values
-      
+
       var oneStars = []
       var twoStars = []
       var threeStars = []
       var fourStars = []
       var fiveStars = []
+
+      // Append characters to their star ratings list
+      // ***Currently no character imgs yet, using a placeholder atm***
       for (var idx = 1; idx < sheetResponse.length; idx++) {
         var characterData = sheetResponse[idx].slice(0, 4)
         var formattedCharacterData = {
@@ -25,9 +38,25 @@ export async function fetchGoogleSheets() {
           numStars: characterData[3],
           type1: characterData[1],
           type2: characterData[2],
-          imgLink: undefined
+          imgLink: ERROR_IMAGE
         }
-        switch(characterData[3]) {
+
+        IMAGE_FORMATS.forEach(function (imgFormat) {
+          const path = IMAGE_PATH + formattedCharacterData.name + imgFormat;
+          fs.exists(path, (exists) => {
+            if (exists) {
+              if (formattedCharacterData.imgLink != ERROR_IMAGE) {
+                console.log("U DONE FUCKED UP IN src\\app\\gacha\\wastemoney\\fetch-sheet.js:fetchGoogleSheets: duplicate images for %s", formattedCharacterData.name);
+              }
+              formattedCharacterData.imgLink = path;
+            }
+          })
+        });
+        if (formattedCharacterData.imgLink === ERROR_IMAGE) {
+          console.log("U DONE FUCKED UP IN src\\app\\gacha\\wastemoney\\fetch-sheet.js:fetchGoogleSheets: no image for %s", formattedCharacterData.name);
+        }
+
+        switch (characterData[3]) {
           case '1':
             oneStars.push(formattedCharacterData)
             break
@@ -55,24 +84,10 @@ export async function fetchGoogleSheets() {
         fourStars: fourStars,
         fiveStars: fiveStars
       }
-      console.log(oneStars)
 
+      // Save character list as a local file
       fs.writeFile(process.env.GACHA_POOL_PATH, JSON.stringify(pool), (err) => {
-          if (err) console.log('Error writing file:', err)
+        if (err) console.log('Error writing file:', err)
       })
-  })
-}
-
-export function getPool(callback) {
-  fs.readFile(process.env.GACHA_POOL_PATH, 'utf8', (err, jsonString) => {
-      if (err) {
-          return
-      }
-      try {
-          const pool = JSON.parse(jsonString)
-          callback(pool)
-  } catch(err) {
-          console.log('Error parsing JSON string:', err)
-      }
-  })
+    })
 }
